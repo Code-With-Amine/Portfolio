b38cc3f9e528246518416edfb32121c34fabd039
Code-With-Amine <userihj36@gmail.com>
2023-06-19 16:58:38 +0100

    I've modified and add two projects

--- js/script.js
+++ js/script.js
@@ -1,23 +1,44 @@
-const images = document.querySelectorAll('.animate');
 
-images.forEach((image) => {
-    image.addEventListener('mouseenter', () => {
-        image.classList.add('rotate');
+document.addEventListener('DOMContentLoaded', () => {
+    const seeMore = document.getElementById('seeMore');
+    const showMoreSkills = document.getElementById('otherSkills');
+
+    seeMore.addEventListener('click', () => {
+        if (showMoreSkills.style.display === 'none') {
+            showMoreSkills.style.display = 'flex';
+            seeMore.textContent = 'See less';
+        } else {
+            showMoreSkills.style.display = 'none';
+            seeMore.textContent = 'See more...';
+        }
     });
 
-});
+    const seeMoreProjects = document.getElementById('seeMoreProjects');
+    const showProjects = document.getElementById('showProjects');
 
-const seeMore = document.getElementById('seeMore');
-seeMore.addEventListener('click',() =>{
-    const showMoreSkills = document.getElementById('otherSkills');
-    showMoreSkills.style.display = "flex";
-    seeMore.classList.add('d-none')
-}
-)
+    seeMoreProjects.addEventListener('click', () => {
+        if (showProjects.style.display === 'none') {
+            showProjects.style.display = 'flex';
+            seeMoreProjects.textContent = 'See less';
+        } else {
+            showProjects.style.display = 'none';
+            seeMoreProjects.textContent = 'See more...';
+        }
+    });
 
-const seeMoreProjects = document.getElementById('seeMoreProjects');
-seeMoreProjects.addEventListener('click',() => {
-    const seeProjects = document.getElementById('showProjects')
-    seeProjects.style.display = "flex";
-    seeMoreProjects.classList.add('d-none')
-})
+    let expandButtons = document.querySelectorAll(".expand-button");
+
+    expandButtons.forEach(function (button) {
+      button.addEventListener("click", function () {
+        let cardText = button.parentNode.querySelector(".card-text");
+  
+        if (button.checked) {
+          cardText.style.webkitLineClamp = "unset";
+         
+        } else {
+          cardText.style.webkitLineClamp = "2";
+        }
+      });
+    });
+
+    });
